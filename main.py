0dcdbb1b8de4b082bdd8bdeafce017abf6e0dcb7
akixuu <75064265+akixuu@users.noreply.github.com>
2022-06-02 08:11:16 +0000

    save

--- main.py
+++ main.py
@@ -4,26 +4,27 @@ import random
 
 # haha i legit just lost 400 lines of code im going to cry
 # bad implementation
-black="\033[1;90m"
-red="\033[0;31m"
-green="\033[0;32m"
-yellow="\033[0;33m"
-blue="\033[0;94m"
-cyan="\033[0;36m"
-white="\033[0;37m"
-bold="\033[1m"
-normal="\033[0m"
+black = "\033[1;90m"
+red = "\033[0;31m"
+green = "\033[0;32m"
+yellow = "\033[0;33m"
+blue = "\033[0;94m"
+cyan = "\033[0;36m"
+
+white = "\033[0;37m"
+bold = "\033[1m"
+normal = "\033[0m"
 
 
 class assets:
-	title=bold+r"""
+    title = bold + r"""
 	___                        ____       
   / _ | __ _  ___  ___  ___ _/ __/_ _____
  / __ |/  ' \/ _ \/ _ \/ _ `/\ \/ // (_-<
 /_/ |_/_/_/_/\___/_//_/\_, /___/\_,_/___/
                       /___/              
-	"""+normal
-	amongus=r"""
+	""" + normal
+    amongus = r"""
 	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀ 
 	⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠛⠉⠙⠛⠛⠛⠛⠻⢿⣿⣷⣤⡀⠀⠀⠀⠀⠀ 
 	⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠋⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠈⢻⣿⣿⡄⠀⠀⠀⠀ 
@@ -44,8 +45,8 @@ class assets:
 	⠀⠀⠀⠀⠀⠀⠀⢿⣿⣦⣄⣀⣠⣴⣿⣿⠁⠀⠈⠻⣿⣿⣿⣿⡿⠏⠀⠀⠀⠀ 
 	⠀⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⠿⠿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 	"""
-	
-	smallsus_and_bigsus_being_wholesome=r"""
+
+    smallsus_and_bigsus_being_wholesome = r"""
 	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣶⣿⣿⣷⣶⣄⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 	⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣾⣿⣿⡿⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀
@@ -80,8 +81,8 @@ class assets:
 	⠀⠀⠀⠀⠀⠀⠀⢿⣿⣦⣄⣀⣠⣴⣿⣿⠁⠀⠈⠻⣿⣿⣿⣿⡿⠏⠀⠀⠀⠀ 
 	⠀⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⠿⠿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 	"""
-	
-	was_impos=r"""
+
+    was_impos = r"""
 	. 　　　。　　　　•　 　ﾟ　　。 　　.
 	
 	　　　.　　　 　　.　　　　　。　　 。　. 　
@@ -94,8 +95,8 @@ class assets:
 	
 	　　ﾟ　　　.　　　. ,　　　　.　          .
 	"""
-	
-	smallsus=r"""
+
+    smallsus = r"""
 	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣶⣿⣿⣷⣶⣄⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 	⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣾⣿⣿⡿⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀
 	⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⡟⠁⣰⣿⣿⣿⡿⠿⠻⠿⣿⣿⣿⣿⣧⠀⠀⠀⠀
@@ -112,138 +113,164 @@ class assets:
 	⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣦⣤⣤⣤⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀
 	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⢿⣿⣿⣿⣿⣿⣿⠿⠋⠉⠛⠋⠉⠉⠁⠀⠀⠀⠀
 	"""
-	tinysus='ඞ'
+    tinysus = 'ඞ'
+
+
 #############################
 # World Assets
 
-user = {
-	'color':'',
-	'tasks completed':0
-}
-impostor = {
-	'color':'',
-	'kill count':0
-}
+user = {'color': '', 'tasks completed': 0}
+impostor = {'color': '', 'kill count': 0}
+
 
 class characters:
-	npcs=['black', 'red', 'green', 'blue', 'yellow', 'cyan']
-	deceased=[]
-	
+    npcs = ['black', 'red', 'green', 'blue', 'yellow', 'cyan']
+    deceased = []
+
+
 class Room:
-	def __init__(self, name, tasks, has_body):
-		self.name = name
-		self.tasks = tasks
-		self.has_body = has_body
+    def __init__(self, name, tasks, has_body):
+        self.name = name
+        self.tasks = tasks
+        self.has_body = has_body
 
 
 rooms = {
-	"med":Room("Medical", 0, False),
-	"elec":Room("Electrical and Engine", 0, False),
-	"sleep":Room("Sleep Chambers", 0, False),
-	"lobby":Room("Main Lobby", 0, False),
-	"nav":Room("Navigation", 0, False),
+    "med": Room("Medical", 0, False),
+    "elec": Room("Electrical and Engine", 0, False),
+    "sleep": Room("Sleep Chambers", 0, False),
+    "lobby": Room("Main Lobby", 0, False),
+    "nav": Room("Navigation", 0, False),
 }
 
+
 class cutscenes:
-	def happy():
-		cut=random.randint(0,1)
-		
-		if cut == 0:
-			print("")
-		
-		if cut == 1:
-			print("")
-		
-		if cut == 2:
-			print("")
-		
-	def sad():
-		cut=random.randint(0,1)
-		
-		if cut == 0:
-			print("")
-		
-		if cut == 1:
-			print("")
-		
-		if cut == 2:
-			print("")
-			
-	def imposter_revealing():
-		cut=random.randint(0,1)
-		
-		if cut == 0:
-			print("")
-			
-		if cut == 1:
-			print("")
-			
-		if cut == 2:
-			print("")
-	
-	def joke():
-		print("")
-	
-	
+    def happy():
+        cut = random.randint(0, 1)
+
+        if cut == 0:
+            print("")
+
+        if cut == 1:
+            print("")
+
+        if cut == 2:
+            print("")
+
+    def sad():
+        cut = random.randint(0, 1)
+
+        if cut == 0:
+            print("")
+
+        if cut == 1:
+            print("")
+
+        if cut == 2:
+            print("")
+
+    def imposter_revealing():
+        cut = random.randint(0, 1)
+
+        if cut == 0:
+            print("")
+
+        if cut == 1:
+            print("")
+
+        if cut == 2:
+            print("")
+
+    def joke():
+        print("")
+
+
 #############################
 
 {
-	# black = "\033[0;30;41m"
-# red = "\033[0;31m"
-# green = "\033[0;32m"
-# yellow = "\033[0;33m"
-# blue = "\033[0;34;47m"
-# cyan = "\033[0;36m"
-# white = "\033[0;37m"
-# purple = "\033[0;35m"
-# bright_black = "\033[1;90m"
-# bright_red = "\033[0;91m"
-# bright_green = "\033[0;92m"
-# bright_yellow = "\033[3;93;44m"
-# bright_blue = "\033[0;94m"
-# bright_purple = "\033[0;95m"
-# bright_cyan = "\033[0;96m"
-# bright_white = "\033[0;97m"
+    # black = "\033[0;30;41m"
+    # red = "\033[0;31m"
+    # green = "\033[0;32m"
+    # yellow = "\033[0;33m"
+    # blue = "\033[0;34;47m"
+    # cyan = "\033[0;36m"
+    # white = "\033[0;37m"
+    # purple = "\033[0;35m"
+    # bright_black = "\033[1;90m"
+    # bright_red = "\033[0;91m"
+    # bright_green = "\033[0;92m"
+    # bright_yellow = "\033[3;93;44m"
+    # bright_blue = "\033[0;94m"
+    # bright_purple = "\033[0;95m"
+    # bright_cyan = "\033[0;96m"
+    # bright_white = "\033[0;97m"
 }
 
+
 # text effects
-class txt_effects:
-	def dotdotdot(delay=0.05):
-		for x in range(3):
-			txt_effects.type_print('.', delay)
-		print("\n")
 
-	def type_print(text, delay=0.05):
-			for character in text:
-					sys.stdout.write(character)
-					sys.stdout.flush()
-					sleep(delay)
-				
-	def color_print(colorcode, text, typed=False, delay=0.05):
+def bolden(txt):
+	return bold + txt + normal
+
+def dotdotdot(delay=0.5):
+	for x in range(3):
+			type('.', delay)
+	print("\n")
+
+def type(text, delay=0.05):
+		for character in text:
+				sys.stdout.write(character)
+				sys.stdout.flush()
+				sleep(delay)
+
+def color_print(colorcode, text, typed=False, delay=0.05):
 		if typed:
-			txt_effects.type_print(colorcode + text + white, delay)
+				type(colorcode + text + white, delay)
 		else:
-			print(colorcode + text + white, end='')
+				print(colorcode + text + white, end='')
 
 
-print(red+assets.title+white+'\n')
+print(red + assets.title + white + '\n')
+print("Welcome to " + red + "AmongSus 2.0" + white +
+      " - a luck based murder mystery.\n\nFirst, select your avatar color. We have red, black, green, yellow, blue, cyan. ", end='')
 
-user["color"]=input("Welcome to "+ red + "AmongSus 2.0" + white + " - a luck based murder mystery. \n\nFirst, select your avatar color. We have red, yellow, green, blue, cyan and black. Take your pick: ")
-while user["color"] not in characters.npcs: 
-	user["color"]=input("Sorry, we don't have that color, or you may have a typo. Please try again: ")
+user["color"] = input(
+    "Take your pick: "
+)
+
+while user["color"] not in characters.npcs:
+    user["color"] = input(
+        "\nSorry, we don't have that color, or you may have a typo. Please try again: "
+    )
 
 print('\n')
-txt_effects.dotdotdot(0.5)
+dotdotdot()
 # remove user color from main npc list
 characters.npcs.remove(user["color"])
 
 print("\nYou have chosen ", end='')
-txt_effects.color_print(eval(user["color"]), user["color"])
+color_print(eval(user["color"]), user["color"])
 print(".\n")
 
-delay(1)
+dotdotdot()
 
-print("You are a astronaut on a mission to space, but recently, there has been suspicious activity going around in the spaceship. Many crew members theorize that there might be a  Your mission is to uncover ")
+bolden("INTRODUCTION")
 
-txt_effects.color_print(eval(user["color"]), assets.tinysus)
-txt_effects.type_print("  Welcome aboard, \033[1mcrewmate.\033[0m")
+type(
+    "You are a astronaut embarking on a mission to another planet, but recently, the spaceship has been breached by a unknown entity. ")
+
+sleep(1)
+
+type("Blending in as a crewmate, the entity is now under diguise. ")
+
+sleep(1)
+
+type("Uncover identity of the " + bolden("IMPOSTOR") +
+      ", and save the crew before it's too late")
+
+dotdotdot(0.5)
+
+sleep(2)
+
+color_print(eval(user["color"]), assets.tinysus)
+type("  Welcome aboard, " + bolden('crewmate') + '.')
